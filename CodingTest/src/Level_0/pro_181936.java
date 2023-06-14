0589da50892cfe36c37bfd9405d69023fcc7a93c
KROOKIMWOOBIN <woodol4540@naver.com>
2023-06-14 15:59:16 +0900

    Day0614

--- CodingTest/src/Level_0/pro_181936.java
+++ CodingTest/src/Level_0/pro_181936.java
@@ -2,8 +2,17 @@ package Level_0;
 
 public class pro_181936 {
 
+	public static int solution(int number, int n, int m) {
+        
+        if(number % n == 0 && number % m == 0) {
+        	return 1;
+        }
+        
+        return 0;
+    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
