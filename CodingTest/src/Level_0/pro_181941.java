a1c5d380dcd640cdc4328349129bb5a161dcec8e
KIMWOOBIN <woodol4540@naver.com>
2023-06-21 14:21:04 +0900

    Day0621

--- CodingTest/src/Level_0/pro_181941.java
+++ CodingTest/src/Level_0/pro_181941.java
@@ -2,9 +2,14 @@ package Level_0;
 
 public class pro_181941 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static String solution(String[] arr) {
+        String answer = "";
+        
+        for(int i = 0; i < arr.length; i++) {
+        	answer += arr[i];
+        }
+        
+        return answer;
+    }
 
 }
