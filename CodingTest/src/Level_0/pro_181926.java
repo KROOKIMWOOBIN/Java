aa54aef63f16ee1802d2f6592d77203dc9740671
KIMWOOBIN <woodol4540@naver.com>
2023-06-25 03:51:11 +0900

    Day0625

---
+++ CodingTest/src/Level_0/pro_181926.java
@@ -0,0 +1,25 @@
+package Level_0;
+
+public class pro_181926 {
+	public static int solution(int n, String control) {
+        int answer = n;
+        
+        for(int i = 0; i < control.length(); i++) {
+        	switch(control.charAt(i)) {
+        	case 'w' :
+        		answer += 1;
+        		break;
+        	case 's' :
+        		answer -= 1;
+        		break;
+        	case 'd' :
+        		answer += 10;
+        		break;
+        	case 'a' :
+        		answer -= 10;
+        		break;
+        	}
+        }
+        return answer;
+    }
+}
