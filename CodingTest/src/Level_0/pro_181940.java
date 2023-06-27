9b2cd85bebb4cd2c0b50a017ddfedafc078edddd
KIMWOOBIN <woodol4540@naver.com>
2023-06-27 09:03:12 +0900

    Day0627

---
+++ CodingTest/src/Level_0/pro_181940.java
@@ -0,0 +1,14 @@
+package Level_0;
+
+public class pro_181940 {
+
+	public static String solution(String my_string, int k) {
+        String answer = "";
+        
+        for(int i = 0; i < k; i++) {
+        	answer += my_string;
+        }
+        
+        return answer;
+    }
+}
