0f7cac55377d2841ef95a7a7182a2cc5e3dc4d6b
KIMWOOBIN <woodol4540@naver.com>
2023-09-14 16:01:51 +0900

    Day0914

--- CodingTest/src/Level_0/pro_181935.java
+++ CodingTest/src/Level_0/pro_181935.java
@@ -1,10 +1,29 @@
 package Level_0;
 
-public class pro_181935 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public class pro_181935 
+{
+	public int solution(int n) 
+	{
+        int answer = 0;
+        
+        for(int i = 1; i <= n; i++)
+        {
+        	if(n % 2 != 0)
+        	{
+        		if(i % 2 != 0)
+        		{
+        			answer += i;
+        		}
+        	}
+        	else
+        	{
+        		if(i % 2 == 0)
+        		{
+        			answer += i * i;
+        		}
+        	}
+        }
+        return answer;
+    }
 
 }
