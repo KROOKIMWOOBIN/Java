c35a624f7a9ce60f3a5635bd578b6c2ee032bd7e
KIMWOOBIN <woodol4540@naver.com>
2023-09-20 22:22:35 +0900

    Day0920

--- CodingTest/src/Level_1/pro_12903.java
+++ CodingTest/src/Level_1/pro_12903.java
@@ -1,10 +1,20 @@
 package Level_1;
 
-public class pro_12903 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class pro_12903 
+{
+	public String solution(String s) 
+	{
+        String answer = "";
+        
+        System.out.println(s.length()/2);
+        if(s.length() % 2 != 0)
+        {
+        	answer += String.valueOf(s.charAt(s.length()/2));
+        }
+        else
+        {
+        	answer = String.valueOf(s.charAt(s.length()/2 - 1)) + String.valueOf(s.charAt(s.length()/2));
+        }
+        return answer;
+    }
 }
