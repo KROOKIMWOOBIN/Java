1072673dd6ce8ba715c4573b241238bbaec731b3
KIMWOOBIN <woodol4540@naver.com>
2023-09-21 11:46:42 +0900

    Day0921

---
+++ CodingTest/src/Level_1/pro12918.java
@@ -0,0 +1,10 @@
+package Level_1;
+
+public class pro12918 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- CodingTest/src/Level_1/pro_12969.java
+++ CodingTest/src/Level_1/pro_12969.java
@@ -1,5 +1,22 @@
 package Level_1;
 
-public class pro_12969 {
+import java.util.*;
 
+public class pro_12969 
+{
+	public static void main(String[] args) 
+	{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i = 0; i < m; i++)
+        {
+        	for(int j = 0; j < n; j++)
+        	{
+        		System.out.print("*");
+        	}
+        	System.out.println("");
+        }
+    }
 }
