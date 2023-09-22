5e28c8d92bfeccc3219dccfdc7f8c70dad88b967
KIMWOOBIN <woodol4540@naver.com>
2023-09-22 10:41:30 +0900

    Day0922

--- CodingTest/src/Level_1/pro12918.java
+++ CodingTest/src/Level_1/pro12918.java
@@ -1,10 +1,21 @@
 package Level_1;
 
-public class pro12918 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class pro12918 
+{
+	public boolean solution(String s) 
+	{
+		if(s.length() == 4 || s.length() == 6)
+		{
+			try 
+	        {
+	        	Integer.parseInt(s);
+	        	return true;
+	        }
+	        catch (NumberFormatException e) {
+				return false;
+			}
+		}
+		else 
+			return false;
+    }
 }
