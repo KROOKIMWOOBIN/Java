c94ed9893b19dd750f1e3bb41cc36a4011dad4bc
KIMWOOBIN <woodol4540@naver.com>
2023-10-02 10:02:55 +0900

    Day1002

---
+++ CodingTest/src/Level_0/pro_181854.java
@@ -0,0 +1,21 @@
+package Level_0;
+
+public class pro_181854 {
+	public int[] solution(int[] arr, int n) {
+        int[] answer = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+        	answer[i] = arr[i];
+        	if(arr.length % 2 != 0) {
+        		if(i % 2 == 0) {
+        			answer[i] = arr[i] + n;
+        		}
+        	}else {
+        		if(i % 2 != 0) {
+        			answer[i] = arr[i] += n;
+        		}
+        	}
+        }
+        
+        return answer;
+    }
+}
